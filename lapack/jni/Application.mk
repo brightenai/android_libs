4c0a161ff3a1081ddf5d2c691a992e7232ca350f
john burkey <johnburkey@mac.com>
2018-07-22 08:55:57 -0500

    Update Application.mk

--- lapack/jni/Application.mk
+++ lapack/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_shared
-APP_CPPFLAGS := -g0 -frtti -fexceptions -mfloat-abi=softfp -mfpu=neon -std=gnu++0x -Wno-deprecated \
+APP_CPPFLAGS := -g0 -O3 -frtti -fexceptions -mfloat-abi=softfp -mfpu=neon -std=gnu++0x -Wno-deprecated \
 -ftree-vectorize -ffast-math -fsingle-precision-constant
 APP_ABI := arm64-v8a
 APP_OPTIM := release
